09e16e1d944554d7964f12546eeb598db076044f
mtvelasquezg <mtvelasquezg@unal.edu.co>
2020-11-27 13:36:07 -0500

    mapping relations

--- src/main/java/com/platzi/market/persistence/entity/Cliente.java
+++ src/main/java/com/platzi/market/persistence/entity/Cliente.java
@@ -1,9 +1,7 @@
 package com.platzi.market.persistence.entity;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table (name = "clientes")
@@ -20,6 +18,8 @@ public class Cliente {
     @Column (name = "correo_electronico")
     private String correoElectronico;
 
+    @OneToMany (mappedBy = "cliente")
+    private List<Compra> compras;
 
     public String getId() {
         return id;
