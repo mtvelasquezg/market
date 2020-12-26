7427c47b5bedd5c770583cb8e6c65888ddec4dca
mtvelasquezg <mtvelasquezg@unal.edu.co>
2020-12-25 19:42:38 -0500

    jwt was implemented

---
+++ src/main/java/com/platzi/market/domain/dto/AuthenticationRequest.java
@@ -0,0 +1,14 @@
+package com.platzi.market.domain.dto;
+
+public class AuthenticationRequest {
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
